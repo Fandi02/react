7270357a1c92dc124dcfb2da8d511289c2d46c6d
Fandi Maulana <fandimaulana936@gmail.com>
2022-05-20 23:35:05 +0700

    website react

--- src/components/ListHandphones.js
+++ src/components/ListHandphones.js
@@ -8,7 +8,8 @@ const ListHandphones = ({ listHandphones }) => {
       <Card>
         <Card.Img variant="top" src="holder.js/100px180" />
         <Card.Body>
-          <Card.Title>{listHandphones.brand_name}</Card.Title>
+          {/* <Card.Title>{listHandphones.brand_name}</Card.Title> */}
+          <Card.Title>Title</Card.Title>
           <Card.Text>
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
